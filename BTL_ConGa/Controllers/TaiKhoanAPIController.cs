fea3c6ef89acad229cee7bcb4791a03f16074104
hieumanh1212 <manhhieu121202@gmail.com>
2023-04-05 17:52:16 +0700

    AfterfixLogin

--- BTL_ConGa/Controllers/TaiKhoanAPIController.cs
+++ BTL_ConGa/Controllers/TaiKhoanAPIController.cs
@@ -31,38 +31,97 @@ namespace BTL_ConGa.Controllers
             return db.TaiKhoans.FirstOrDefault(x => x.TaiKhoan1 == maLoai);
         }
 
+        //[HttpPost]
+        //public bool DangKy(string taikhoan1, string matkhau, string maloaitaikhoan, string idkhachhang, 
+        //    string tenkhachhang, string ngaysinh, string sodienthoai, string diachi, string email, 
+        //    string gioitinh, string taikhoan2)
+        //{
+        //    try
+        //    {
+        //        var checkUsernameExsit = db.TaiKhoans.FirstOrDefault(x=>x.TaiKhoan1==taikhoan1);
+        //        //Tai khoan da ton tai
+        //        if(checkUsernameExsit != null)
+        //        {
+        //            return false;
+        //        }
+        //        else
+        //        {
+        //            TaiKhoan taikhoan = new TaiKhoan();
+        //            taikhoan.TaiKhoan1 = taikhoan1;
+        //            taikhoan.MatKhau = matkhau;
+        //            taikhoan.MaLoaiTaiKhoan = maloaitaikhoan;
+
+        //            db.TaiKhoans.AddAsync(taikhoan);
+        //            db.SaveChanges();
+
+        //            return true;
+        //        }
+
+        //    }
+        //    catch
+        //    {
+        //        throw new Exception("Đăng kí không thành công"); 
+        //    }
+        //}
+
         [HttpPost]
-        public bool DangKy(string taikhoan1, string matkhau, string maloaitaikhoan, string idkhachhang, 
-            string tenkhachhang, string ngaysinh, string sodienthoai, string diachi, string email, 
-            string gioitinh, string taikhoan2)
+        [Route("DangKy")]
+        public async Task<IActionResult> DangKy([FromBody] TaiKhoanIn4Model taikhoan)
         {
             try
             {
-                var checkUsernameExsit = db.TaiKhoans.FirstOrDefault(x=>x.TaiKhoan1==taikhoan1);
+                var checkUsernameExsit = db.TaiKhoans.FirstOrDefault(x => x.TaiKhoan1 == taikhoan.Username);
                 //Tai khoan da ton tai
-                if(checkUsernameExsit != null)
+                if (checkUsernameExsit != null)
                 {
-                    return false;
+                    return BadRequest();
                 }
                 else
                 {
-                    TaiKhoan taikhoan = new TaiKhoan();
-                    taikhoan.TaiKhoan1 = taikhoan1;
-                    taikhoan.MatKhau = matkhau;
-                    taikhoan.MaLoaiTaiKhoan = maloaitaikhoan;
-
-                    db.TaiKhoans.AddAsync(taikhoan);
-                    db.SaveChanges();
-
-                    return true;
+                    try
+                    {
+                        await _userInforService.Register(taikhoan);
+                        return Ok();
+                    }
+                    catch
+                    {
+                        throw new Exception("hhh");
+                    }
                 }
-                
+
             }
             catch
             {
-                throw new Exception("Đăng kí không thành công"); 
+                throw new Exception("Đăng kí không thành công");
             }
         }
-        
+
+
+        [HttpPost]
+        [Route("DangNhap")]
+        public IActionResult DangNhap([FromBody] TaiKhoanOnlyModel taikhoan)
+        {
+            try
+            {
+                var checkUsernameExsit = db.TaiKhoans.FirstOrDefault(x => x.TaiKhoan1 == taikhoan.Username && x.MatKhau == taikhoan.Password 
+                && x.MaLoaiTaiKhoan == taikhoan.AccountType);
+                //Tai khoan da ton tai
+                if (checkUsernameExsit != null)
+                {
+                    return Ok();                   
+                    //return RedirectToAction("TrangChu", "TrangChu");
+                }
+                else
+                {
+                    return BadRequest();
+                    //return View();
+                }
+
+            }
+            catch
+            {
+                throw new Exception("Đăng nhập không thành công");
+            }
+        }
     }
 }
